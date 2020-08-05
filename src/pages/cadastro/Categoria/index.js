252587ac3697368913b8cf13fd4c32f89dc2207e
Igor Szot <szottt@hotmail.com>
2020-08-05 14:00:22 -0300

    Ajuste do formulario

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,16 @@ function CadastroCategoria(){
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
 
+        <form>
+          Nome da Categoria
+          <input 
+            type='text'
+            />
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
         <Link to="/" >
             Ir Para Home
         </Link>
