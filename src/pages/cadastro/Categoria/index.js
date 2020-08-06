3ad22886c93435fb2b4e011acdb071020ff3f810
Igor Szot <szottt@hotmail.com>
2020-08-06 10:56:34 -0300

    Alterações com servidor Local

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -26,6 +26,18 @@ function CadastroCategoria() {
       infosDoEvento.target.value,
     );
   }
+
+  useEffect(() => {
+    console.log('teste');
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  }, []);
 
   return (
     <PageDefault>
@@ -75,6 +87,12 @@ function CadastroCategoria() {
 
       </form>
 
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+
       <ul>
         {categorias.map((categoria, indice) => (
           // eslint-disable-next-line react/no-array-index-key
@@ -85,9 +103,8 @@ function CadastroCategoria() {
       </ul>
 
       <Link to="/">
-        Ir Para Home
+        Ir para home
       </Link>
-
     </PageDefault>
   );
 }
