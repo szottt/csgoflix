5b6973d4c81783080d0308f3acad6ef6d39c2dcb
Igor Szot <szottt@hotmail.com>
2020-08-07 14:49:47 -0300

    Ajustes nos nomes

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Header from '../header';
+import Header from '../Header';
 import Footer from '../Footer';
 
 const Main = styled.main`
@@ -13,7 +13,7 @@ const Main = styled.main`
   padding-left: 5%;
   padding-right: 5%;
   ${({ paddingAll }) => css`
-    padding: ${paddingAll}} 
+    padding: ${paddingAll}}
   `}
 `;
 
